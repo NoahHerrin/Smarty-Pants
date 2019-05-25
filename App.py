4b9a514d5c22d4e51b05006256048765c9756995
Noah Herrin <noahherrin@gmail.com>
2019-05-25 16:27:35 -0700

    added graph datastructure, simple program for reading excel data, and a wrapper for the mongodb database

---
+++ App.py
@@ -0,0 +1,14 @@
+from BetterXLRD.SpreadSheet import SpreadSheet
+from Dijkstra.Graph import Graph
+from SimpleDB.MongoDB import database
+# Ideas
+# Use excel spreadsheets to store relationships between items
+# Columns represent origins rows represent destinations
+#    A   B  C
+# A -1  -1  -1
+# B  1  -1  -1
+# C  4  1   -1
+#
+#
+#
+print("Hello World!")
